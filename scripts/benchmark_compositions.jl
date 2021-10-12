212f61a4319a305836a32cfb126d893a686f2b0e
Khalil Chrit <ch.khalil21@gmail.com>
2021-10-13 00:31:50 +0100

    added script for benchmarking generated compositions

---
+++ scripts/benchmark_compositions.jl
@@ -0,0 +1,92 @@
+
+using ICNBenchmarks
+
+#include(joinpath(projectdir("src"), "search_space.jl"))
+
+
+function main()
+    comps = Dict{Any,Any}()
+    mkpath(datadir("composition_results"))
+    for file_name in cd(readdir,joinpath(datadir("compositions")))
+        if startswith(file_name,"con=") 
+            json = JSON.parsefile(joinpath(datadir("compositions"),file_name))
+            counter = 1
+            while (haskey(json, string(counter)))
+                path = joinpath(datadir("composition_results"), file_name)
+                path = "$(path[1:end-5])_$counter.json"
+                if isfile(path)
+                    @warn "The result file already exist" file_name
+                else
+                    concept, comp, selection_rate,
+                     dom_size, search, complete_search_limit,
+                      solutions_limit , param = extract_data_from_json(json, counter)
+                    
+                    solutions, non_sltns, _ = search_space(
+                    dom_size,
+                    concept,
+                    param;
+                    search=search,
+                    complete_search_limit=complete_search_limit,
+                    solutions_limit=solutions_limit,
+                    )
+                    
+                    results = @timed loss(solutions, non_sltns, comp, metric, dom_size, param; samples=nothing)
+                    push!(comps, "composition" => comp)
+                    push!(comps, "composition_number" => counter)
+                    push!(comps, "selection_rate" => selection_rate)
+                    push!(comps, "time" => results.time)
+                    push!(comps, "accuracy" => results.value)
+                    export_compositions(comps, path)
+                    counter += 1
+                end
+            end
+        end
+    end
+end
+
+function extract_data_from_json(file, counter)
+    concept = file["params"]["concept"][1]
+    comp = file[string(counter)]["Julia"]
+    selection_rate = file[string(counter)]["selection_rate"]
+    dom_size = file["params"]["domains_size"]
+    search = eval(Meta.parse(":"*file["params"]["search"]))
+    complete_search_limit = file["params"]["complete_search_limit"]
+    solutions_limit = file["params"]["sampling"]
+    param = generate_param(file["params"]["concept"][2])
+    
+    return concept, comp, selection_rate, dom_size,
+     search, complete_search_limit, solutions_limit, param
+end
+
+function generate_param(param)
+    # Generate an appropriate parameter for the concept if relevant
+    param = if isnothing(param)
+        nothing
+    elseif param == 1
+        rand(1:dom_size)
+    else
+        rand(1:dom_size, param)
+    end
+    return param
+end
+
+
+function export_compositions(comps, path)
+    touch(path)
+    write(path, JSON.json(comps,2))
+end
+
+# Here I'm not sure if the formula σ is correct to calculate precision as a %
+# since the term precision I'm familiar with is a classification metric
+function loss(solutions, non_sltns, composition, metric, dom_size, param; samples=nothing)
+    X = if isnothing(samples)
+        Iterators.flatten((solutions, non_sltns))
+    else
+        Iterators.flatten((solutions, rand(non_sltns, samples)))
+    end
+    σ = sum(x -> 1-(abs(composition(x; param, dom_size) - metric(x, solutions)))/dom_size, X)
+    return σ/length(X)
+end
+
+
+main()
