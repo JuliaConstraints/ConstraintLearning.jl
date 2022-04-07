03dc82fa5e6bab77e454bfa3540525e4c77bfef0
azzaare <jf@baffier.fr>
2022-04-07 12:45:47 +0900

    Add basic structures, tests, and ICN call

--- src/ConstraintLearning.jl
+++ src/ConstraintLearning.jl
@@ -1,5 +1,27 @@
 module ConstraintLearning
 
-# Write your package code here.
+using ConstraintDomains
+using CompositionalNetworks
+using QUBOConstraints
+
+export icn
+export qubo
+
+function qubo(domains, concept, param, ml, ::Val{:ga})
+    f = icn(domains, concept, param, Val(:ga))
+    return qubo(domains, f, param; ml)
+end
+
+function qubo(domains, f, param = nothing; icn = :none, ml = :descent)
+    return nothing
+end
+
+function icn(domains, concept, param, ::Val{:ga})
+    return composition(explore_learn_compose(domains, concept, param)[1])
+end
+
+function icn(domains, concept, param = nothing; optim = :ga)
+    return icn(domains, concept, param, Val(optim))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
+using ConstraintDomains
 using ConstraintLearning
 using Test
 
 @testset "ConstraintLearning.jl" begin
-    # Write your tests here.
+    domains = [domain([1,2,3,4]) for i in 1:4]
+    compo = icn(domains, allunique)
+    @test compo([1,2,3,3], dom_size = 4) > 0.0
+
 end
