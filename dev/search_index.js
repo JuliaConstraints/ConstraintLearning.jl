efde499f13d596c8aad6f603dc70a96d0597d4a6
Documenter.jl <documenter@juliadocs.github.io>
2022-04-15 13:35:28 +0000

    build based on d8ac12e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConstraintLearning","category":"page"},{"location":"#ConstraintLearning","page":"Home","title":"ConstraintLearning","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConstraintLearning.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConstraintLearning]","category":"page"}]
+}
