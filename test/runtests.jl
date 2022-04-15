7654073f973b49c019115327f8b9610a41cd1b95
azzaare <jf@baffier.fr>
2022-04-15 22:15:22 +0900

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,23 +12,63 @@ end
 
 @testset "QUBOConstraints.jl" begin
     println("\nQUBO: Test for All Different 3×3")
-    all_different(x) = allunique(x)
+    X₃₃ = [rand(0:2, 3) for _ in 1:10]
+    X₃₃_test = [rand(0:2, 3) for _ in 1:100]
 
-    binary_all_different(x) = all_different(integerize(x))
+    B₉ = [rand(Bool, 9) for _ in 1:10]
+    B₉_test = [rand(Bool, 9) for _ in 1:2000]
 
-    opt_all_different(x) = 0.
+    training_configs = [
+        Dict(
+            :info => "No binarization on ⟦0,2⟧³",
+            :train => X₃₃,
+            :test => X₃₃_test,
+            :encoding => :none,
+            :binarization => :none,
+        ),
+        Dict(
+            :info => "Domain Wall binarization on ⟦0,2⟧³",
+            :train => X₃₃,
+            :test => X₃₃_test,
+            :encoding => :none,
+            :binarization => :domain_wall,
+        ),
+        Dict(
+            :info => "One-Hot pre-encoded on ⟦0,2⟧³",
+            :train => B₉,
+            :test => B₉_test,
+            :encoding => :one_hot,
+            :binarization => :none,
+        ),
+    ]
 
-    function sat_all_different(X)
-        if count(x -> x == sqrt(length(X)), integerize(X)) > 0
-            return sqrt(length(X))
-        elseif binary_all_different(X)
-            return -1.
+    function all_different(x, encoding)
+        encoding == :none && (return allunique(x))
+        isv = if encoding == :one_hot
+            mapreduce(i -> is_valid(x[i:i+2], Val(encoding)), *, 1:3:9)
         else
-            return 1.
+            mapreduce(i -> is_valid(x[i:i+1], Val(encoding)), *, 1:2:6)
+        end
+        if isv
+            b = all_different(debinarize(x; binarization = encoding), :none)
+            return b ? -1. : 1.
+        else
+            return length(x)
         end
     end
 
-    X_train = [collect(binarize(rand(0:2, 3))) for _ in 1:100]
-    X_check = [collect(binarize(rand(0:2, 3))) for _ in 1:10000]
-    qubo(X_train, sat_all_different; X_check)
+    function all_different(x, encoding, binarization)
+        return all_different(x, encoding == :none ? binarization : encoding)
+    end
+
+    for config in training_configs
+        @testset "$(config[:info])" begin
+            println("\nTest for $(config[:info])")
+            penalty = x -> all_different(x, config[:encoding], config[:binarization])
+            train(
+                config[:train], penalty;
+                binarization = config[:binarization], X_test = config[:test]
+            )
+        end
+    end
 end
