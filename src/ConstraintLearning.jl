9bdf29bc503b2111f4667e6040d5e152807824f7
azzaare <jf@baffier.fr>
2022-09-18 12:24:23 +0900

    Add abstract optimizer

--- src/ConstraintLearning.jl
+++ src/ConstraintLearning.jl
@@ -1,12 +1,15 @@
 module ConstraintLearning
 
+# SECTION - imports
 using ConstraintDomains
 using CompositionalNetworks
 using QUBOConstraints
 
+# SECTION - usings
 export icn
 export qubo
 
+# SECTION - includes
 include("common.jl")
 include("icn.jl")
 include("qubo.jl")
