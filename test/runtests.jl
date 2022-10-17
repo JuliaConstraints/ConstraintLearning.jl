3c9f51a4b14c857cec58ce8726f8f483e3a06589
azzaare <jf@baffier.fr>
2022-10-17 18:02:47 +0900

    Reactivate all tests. Tag new version

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,12 @@ using ConstraintLearning
 using QUBOConstraints
 using Test
 
-# @testset "CompositionalNetworks.jl" begin
-#     println("\nICN: Test for All Different 4×4")
-#     domains = [domain([1,2,3,4]) for i in 1:4]
-#     compo = icn(domains, allunique)
-#     @test compo([1,2,3,3], dom_size = 4) > 0.0
-# end
+@testset "CompositionalNetworks.jl" begin
+    println("\nICN: Test for All Different 4×4")
+    domains = [domain([1,2,3,4]) for i in 1:4]
+    compo = icn(domains, allunique)
+    @test compo([1,2,3,3], dom_size = 4) > 0.0
+end
 
 @testset "QUBOConstraints.jl" begin
     println("\nQUBO: Test for All Different 3×3")
