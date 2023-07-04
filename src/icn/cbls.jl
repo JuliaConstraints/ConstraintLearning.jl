06088255828e24248713874abec52fe010d19489
Azzaare <jf@baffier.fr>
2023-07-04 10:22:45 +0900

    changes for bench

--- src/icn/cbls.jl
+++ src/icn/cbls.jl
@@ -6,7 +6,7 @@ Default constructor to learn an ICN through a CBLS solver.
 struct ICNLocalSearchOptimizer <: ICNOptimizer
     options::LocalSearchSolvers.Options
 
-    ICNLocalSearchOptimizer(options = LocalSearchSolvers.Options()) = new(options)
+    ICNLocalSearchOptimizer(options=LocalSearchSolvers.Options()) = new(options)
 end
 
 """
@@ -25,14 +25,14 @@ end
 
 Constraint ensuring that at least one operation is selected.
 """
-no_empty_layer(x; X = nothing) = max(0, 1 - sum(x))
+no_empty_layer(x; X=nothing) = max(0, 1 - sum(x))
 
 """
     parameter_specific_operations(x; X = nothing)
 
 Constraint ensuring that at least one operation related to parameters is selected if the error function to be learned is parametric.
 """
-parameter_specific_operations(x; X = nothing) = 0.0
+parameter_specific_operations(x; X=nothing) = 0.0
 
 """
     CompositionalNetworks.optimize!(icn, solutions, non_sltns, dom_size, metric, optimizer::ICNLocalSearchOptimizer; parameters...)
@@ -43,7 +43,7 @@ function CompositionalNetworks.optimize!(
     icn, solutions, non_sltns, dom_size, metric, optimizer::ICNLocalSearchOptimizer; parameters...
 )
     @debug "starting debug opt"
-    m = model(; kind = :icn)
+    m = model(; kind=:icn)
     n = nbits(icn)
 
     # All variables are boolean
@@ -57,7 +57,7 @@ function CompositionalNetworks.optimize!(
     for layer in layers(icn)
         if exclu(layer)
             stop = start + nbits_exclu(layer) - 1
-            f(x; X = nothing) = mutually_exclusive(layer, x)
+            f(x; X=nothing) = mutually_exclusive(layer, x)
             constraint!(m, f, start:stop)
         else
             stop = start + length(layer) - 1
@@ -76,13 +76,13 @@ function CompositionalNetworks.optimize!(
         S = Iterators.flatten((solutions, non_sltns))
         @debug _w compo f S metric
         σ = sum(x -> abs(f(x; X=inplace, dom_size, parameters...) - eval(metric)(x, solutions)), S)
-        return  σ + regularization(icn) + weigths_bias(_w)
+        return σ + regularization(icn) + weigths_bias(_w)
     end
 
     objective!(m, fitness)
 
     # Create solver and solve
-    s = solver(m; options = optimizer.options)
+    s = solver(m; options=optimizer.options)
     solve!(s)
     @debug "pool" s.pool best_values(s.pool) best_values(s) s.pool.configurations
 
@@ -90,14 +90,15 @@ function CompositionalNetworks.optimize!(
     best = BitVector(collect(best_values(s)))
     weigths!(icn, best)
 
-    return best, Dictionary{BitVector, Int}([best], [1])
+    return best, Dictionary{BitVector,Int}([best], [1])
 end
 
-@testitem "ICN: CBLS" tags = [:icn, :cbls] default_imports=false begin
+@testitem "ICN: CBLS" tags = [:icn, :cbls] default_imports = false begin
     using ConstraintDomains
     using ConstraintLearning
+    using Test
 
-    domains = [domain([1,2,3,4]) for i in 1:4]
-    compo = icn(domains, allunique; optimizer = ICNLocalSearchOptimizer())
-    # @test compo([1,2,3,3], dom_size = 4) > 0.0
+    domains = [domain([1, 2, 3, 4]) for i in 1:4]
+    compo = icn(domains, allunique; optimizer=ICNLocalSearchOptimizer())
+    @test compo([1, 2, 3, 3], dom_size=4) > 0.0
 end
